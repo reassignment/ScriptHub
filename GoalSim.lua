c610f7e61f2e2663f36719a7ff6c3f65e226eb03
reassignment <52657016+reassignment@users.noreply.github.com>
2023-01-30 17:44:27 +0100

    New Library

--- GoalSim.lua
+++ GoalSim.lua
@@ -1,30 +1,33 @@
-local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/astraln/SentinelUILIB/main/UI.lua', true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/insanedude59/UILib/main/source"))();
 
-local window = Library:Window('ScriptHub - Goal Simulator')
+lib:SetTitle("ScriptHub - Goal Simulator")
 
-local tab = window:Tab('Automatic')
+local tab1 = lib:NewTab("Automatic", "Automate Stuff")
 
-tab:Toggle('Auto Score Perfect',false, function(t)
-    if t == true then
+
+tab1:NewToggle("Auto Score Perfect",false,function(value)
+   if value == true then
         getgenv().autoPerfect = true;
         while autoPerfect == true do
             game:GetService("ReplicatedStorage").Events.RemoteEvent:FireServer(table.unpack({    [1] = "Football",    [2] = "Shoot",    [3] = 0.02999262628634969,    [4] = false,    [5] = workspace.Goals.Left,    [6] = 11,}))
             game:GetService("ReplicatedStorage").Events.RemoteEvent:FireServer(table.unpack({    [1] = "Football",    [2] = "RewardPlayer",    [3] = false,    [4] = 135,    [5] = workspace.Goals.Left.Destinations["1"].Left,    [6] = 11,    [7] = 1,}))
             wait()
         end
-    elseif t == false then
+    elseif value == false then
         getgenv().autoPerfect = false;
     end
 end)
 
-tab:Toggle('Auto Contract',false, function(t)
-    if t == true then
+tab1:NewToggle("Auto Contract",false,function(value)
+    if value == true then
         getgenv().autoContract = true;
         while autoContract == true do
             game:GetService("ReplicatedStorage").Events.RemoteFunction:InvokeServer(table.unpack({    [1] = "Divisions",    [2] = "AcceptContract",}))
             wait()
         end
-    elseif t == false then
+    elseif value == false then
         getgenv().autoContract = false;
     end
 end)
+
+
