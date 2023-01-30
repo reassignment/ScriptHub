96e43b2f4a891049ae58ec0343af12de0a9db821
Tr3nd1ngT0p1c <117656022+Tr3nd1ngT0p1c@users.noreply.github.com>
2023-01-30 11:08:29 -0600

    Update autoFarmCod.lua

--- autoFarmCod.lua
+++ autoFarmCod.lua
@@ -1,1 +1,7 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/insanedude59/UILib/main/source"))();
+
+lib:SetTitle("ScriptHub - Goal Simulator")
+
+local tab1 = lib:NewTab("Automatic", "Automate Stuff")
+
+tab1:NewToggle('Auto Kill')
