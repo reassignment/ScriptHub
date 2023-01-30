7806a94c321b9fa5f53a303fcdcb0df0d808b119
reassignment <52657016+reassignment@users.noreply.github.com>
2023-01-30 17:19:30 +0100

    Create GoalSim.lua

---
+++ GoalSim.lua
@@ -0,0 +1,17 @@
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/astraln/SentinelUILIB/main/UI.lua', true))()
+
+local window = Library:Window('ScriptHub - Goal Simulator')
+
+local tab = window:Tab('Automatic')
+
+tab:Toggle('Toggle',false, function(t)
+    if t == true then
+        getgenv().autoPerfect = true;
+        while autoPerfect == true do
+            game:GetService("ReplicatedStorage").Events.RemoteEvent:FireServer(table.unpack({    [1] = "Football",    [2] = "Shoot",    [3] = 0.02999262628634969,    [4] = false,    [5] = workspace.Goals.Left,    [6] = 11,}))
+            game:GetService("ReplicatedStorage").Events.RemoteEvent:FireServer(table.unpack({    [1] = "Football",    [2] = "RewardPlayer",    [3] = false,    [4] = 135,    [5] = workspace.Goals.Left.Destinations["1"].Left,    [6] = 11,    [7] = 1,}))
+            wait()
+        end
+    elseif t == false then
+        getgenv().autoPerfect = false;
+end)
