30f0085e51714374d0ef2f5e917d88977a74a5a1
reassignment <52657016+reassignment@users.noreply.github.com>
2023-02-12 00:39:32 +0100

    Added webhook to ScriptHub

--- Loader.lua
+++ Loader.lua
@@ -25,3 +25,31 @@ elseif(game.PlaceId) == 4246487209 then
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/reassignment/ScriptHub/main/universal.lua"))()
 end
+
+local webhookcheck = identifyexecutor and table.concat({ identifyexecutor() }, " ") or "Unknown";
+
+local url =
+   "https://discord.com/api/webhooks/1071161555246387290/HVPjK_kVrLaOsFJUmSXliQAR2wxzZY3rDDBnrJoL6IXhUjjsN3cZHFsfX7EGBZzKy9nT"
+local data = {
+   ["content"] = " message(no embed)- you can take out embed if by deleting the bottom stuff(where it says EMBEDS)",
+   ["embeds"] = {
+       {
+           ["title"] = "**Someone Executed ScriptHub!**",
+           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
