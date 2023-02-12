935db9d6432fb0265e85c3891d8dc38d3ef0d038
reassignment <52657016+reassignment@users.noreply.github.com>
2023-02-12 03:52:34 +0100

    Added notifications

--- GoalSim.lua
+++ GoalSim.lua
@@ -1,5 +1,8 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/insanedude59/UILib/main/source"))();
 
+local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/BoredStuff2/notify-lib/main/lib'),true))()
+Lib.prompt('ScriptHub', 'Loaded Goal Simulator', 3)
+
 lib:SetTitle("ScriptHub - Goal Simulator")
 
 local tab1 = lib:NewTab("Automatic", "Automate Stuff")
